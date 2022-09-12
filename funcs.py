6b536a8c236d567bcad9a9a1839ffe260963bfec
DennisKretz <unrealfaceit2020@gmail.com>
2022-09-12 19:35:46 +0200

    remove options, remove not needed funcs and output, rename funcs

--- funcs.py
+++ funcs.py
@@ -8,7 +8,7 @@ def get_web_content():
     web_content = req.content
     return web_content
 
-def check_if_keys_available(content):
+def get_keys(content):
     scraper = BeautifulSoup(content, 'html.parser')
     for key in scraper.find_all('span', {'class': 'tag'}):
         element = key.find('i').getText()
@@ -16,12 +16,7 @@ def check_if_keys_available(content):
             keys = key.getText()
             if ('0' not in keys):
                 keys = keys.replace('key', '')
-                print("A giveaway is running right now!")
                 return keys
-
-def start_notify(keys, option):
-    if (option == 1):
-        start_windows_notify(keys)
 
 def start_windows_notify(keys):
     notify = ToastNotifier()
@@ -31,12 +26,3 @@ def start_windows_notify(keys):
         duration = 20,
         threaded = True,
     )
-
-def settings():
-    print("[1] windows notify")
-    option = input("Please enter a option, this is for the notify: ")
-    if (option == str(1)):
-        return 1
-    else:
-        print("wrong option - try again")
-        settings()
