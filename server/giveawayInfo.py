6bfd306c6fd9d9a88d14370f2ab7a3234e0d9a8b
DennisKretz <unrealfaceit2020@gmail.com>
2022-09-12 22:30:24 +0200

    add viewers data

--- server/giveawayInfo.py
+++ server/giveawayInfo.py
@@ -20,3 +20,12 @@ class GiveawayInfo:
                 self._keys = key.getText()
                 self._keys = self._keys.replace('key', '')
                 return self._keys.replace(' ', '')
+
+    def get_viewers(self, content):
+        self._scraper = BeautifulSoup(content, 'html.parser')
+        for viewer in self._scraper.find_all('span', {'class': 'tag'}):
+            self._element = viewer.find('i').getText()
+            if (self._element == 'visibility'):
+                self._viewers = viewer.getText()
+                self._viewers = self._viewers.replace('visibility', '')
+                return self._viewers.replace(' ', '')
